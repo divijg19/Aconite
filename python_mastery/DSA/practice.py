99138727c608a69825083ec36258bb9ca716a918
Divij Ganjoo <divijganjoo2003@gmail.com>
2025-12-21 17:38:25 +0530

    DSA prep

---
+++ python_mastery/DSA/practice.py
@@ -0,0 +1,38 @@
+x = 10
+y = 3.5
+name = "Divij"
+is_active = True
+
+print(x)
+print(y)
+print("-----")
+print(name)
+print(is_active)
+
+print("-----")
+
+age=21
+height=175.5
+student=True
+
+print(age)
+print(height)
+print(student)
+print("-----")
+if x > 5:
+    print("x is greater than 5")
+else:
+    print("x is 5 or less")
+
+if age >= 18:
+    print("Adult")
+else:
+    print("Minor")
+
+while x > 0:
+    print(x)
+    x -= 1
+print("Liftoff!")
+
+for i in range(int(y), 20, 2):
+    print(i)
