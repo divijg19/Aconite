d69e8dddf5c6f93e1bfcefafdca1bf0bd7ce3b12
Divij Ganjoo <divijganjoo2003@gmail.com>
2025-12-27 05:45:54 +0530

    leetcode

--- python_mastery/DSA/leetcode/#1652_defuse_the_bomb.py
+++ python_mastery/DSA/leetcode/#1652_defuse_the_bomb.py
@@ -2,17 +2,32 @@
 # https://leetcode.com/problems/defuse-the-bomb/
 
 def decrypt(code, k):
-    n = len(code)
-    if k == 0:
-        return [0] * n
-    result = []
-    for i in range(n):
-        total = 0
+        n = len(code)
+        if k == 0:
+            return [0] * n
+        res = []
+        decrypt = 0
         if k > 0:
-            for j in range(1, k + 1):
-                total += code[(i + j) % n]
-        else:
-            for j in range(1, abs(k) + 1):
-                total += code[(i - j + n) % n]
-        result.append(total)
-    return result
+            for i in range(k):
+                if (i + 1) > k:
+                    decrypt += code[(i + 1) % n]
+                else:
+                    decrypt += code[i + 1]
+            res.append(decrypt)
+
+            for i in range(1, n):
+                decrypt -= code[i]
+                decrypt += code[(i + k) % n]
+                res.append(decrypt)
+            return res
+
+        if k < 0:
+            for i in range(0, abs(k)):
+                decrypt += code[n - i - 1]
+            res.append(decrypt)
+
+            for i in range(1, n):
+                decrypt -= code[(i - 1 + k)]
+                decrypt += code[i - 1]
+                res.append(decrypt)
+            return res
