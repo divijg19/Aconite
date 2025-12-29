77ad800f4d7e452dc44f1b9f78e186e5873d8ad3
Divij Ganjoo <divijganjoo2003@gmail.com>
2025-12-29 21:58:17 +0530

    leetcode

---
+++ python_mastery/DSA/leetcode/#1662_check_if_two_string_arrays_are_equivalent.py
@@ -0,0 +1,5 @@
+# 1662. Check if Two String Arrays are Equivalent
+# https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/
+
+def arrayStringsAreEqual(word1, word2):
+    return ''.join(word1) == ''.join(word2)
