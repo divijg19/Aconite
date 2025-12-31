15b61fff4d3364b1b0f4e57d230060b8cf7f6cd9
Divij Ganjoo <divijganjoo2003@gmail.com>
2025-12-31 19:16:03 +0530

    leetcode

--- python_mastery/DSA/leetcode/#1207_unique_number_of_occurences.py
+++ python_mastery/DSA/leetcode/#1207_unique_number_of_occurences.py
@@ -2,3 +2,17 @@
 # https://leetcode.com/problems/unique-number-of-occurrences/
 
 def uniqueOccurrences(arr):
+    freq = {}
+    for num in arr:
+        if num in freq:
+            freq[num] += 1
+        else:
+            freq[num] = 1
+            
+    occurrences = set()
+    for count in freq.values():
+        if count in occurrences:
+            return False
+        occurrences.add(count)
+        
+    return True
