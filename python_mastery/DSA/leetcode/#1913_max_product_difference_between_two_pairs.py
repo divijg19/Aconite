3d0f6c40c6bd5d96f76565aef4c9a6dcddd71939
divijg19 <divijganjoo2003@gmail.com>
2026-01-06 23:00:45 +0530

    leetcode

--- python_mastery/DSA/leetcode/#1913_max_product_difference_between_two_pairs.py
+++ python_mastery/DSA/leetcode/#1913_max_product_difference_between_two_pairs.py
@@ -1,8 +1,26 @@
 # 1913. Max Product Difference Between Two Pairs
 # https://leetcode.com/problems/max-product-difference-between-two-pairs/
 
+from math import inf
+
 
 def max_product_difference(nums):
-    nums.sort()
+    biggest = 0
+    second_biggest = 0
+    smallest = inf
+    second_smallest = inf
 
-    return nums[-1] * nums[-2] - nums[0] * nums[1]
+    for num in nums:
+        if num > biggest:
+            second_biggest = biggest
+            biggest = num
+        else:
+            second_biggest = max(second_biggest, num)
+
+        if num < smallest:
+            second_smallest = smallest
+            smallest = num
+        else:
+            second_smallest = min(second_smallest, num)
+
+    return biggest * second_biggest - smallest * second_smallest
