819589c1a55600c3be1f9bdae03ba77b25ea9b79
divijg19 <divijganjoo2003@gmail.com>
2026-01-11 23:25:34 +0530

    leetcode

---
+++ python_mastery/DSA/leetcode/#922_sort_array_by_parity_II.py
@@ -0,0 +1,18 @@
+# 922. Sort Array By Parity II
+# https://leetcode.com/problems/sort-array-by-parity-ii/
+
+def sort_array_by_parity_ii(nums):
+    even_index, odd_index = 0, 1
+    n = len(nums)
+
+    while even_index < n and odd_index < n:
+        if nums[even_index] % 2 == 0:
+            even_index += 2
+        elif nums[odd_index] % 2 == 1:
+            odd_index += 2
+        else:
+            nums[even_index], nums[odd_index] = nums[odd_index], nums[even_index]
+            even_index += 2
+            odd_index += 2
+
+    return nums
