0220bda4192912fc7569c3df1dac7d2066438aec
divijg19 <divijganjoo2003@gmail.com>
2026-02-03 21:35:03 +0530

    leetcode

---
+++ python_mastery/DSA/leetcode/#28_find_the_index_of_the_first_occurence_in_array.py
@@ -0,0 +1,17 @@
+# 28. Find the Index of the First Occurrence in Array
+# https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-array/
+
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if not needle:
+            return 0
+
+        needle_length = len(needle)
+        haystack_length = len(haystack)
+
+        for i in range(haystack_length - needle_length + 1):
+            if haystack[i : i + needle_length] == needle:
+                return i
+
+        return -1
