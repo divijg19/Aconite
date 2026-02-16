eac5a305b38e0aeb72a39f7dce42645b5ab0482a
divijg19 <divijganjoo2003@gmail.com>
2026-02-16 21:29:03 +0530

    leetcode

---
+++ Leetcode/DSA/#202_happy_number.py
@@ -0,0 +1,20 @@
+# 202. Happy Number
+# https://leetcode.com/problems/happy-number/
+
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        def get_next(num):
+            total_sum = 0
+            while num > 0:
+                digit = num % 10
+                total_sum += digit**2
+                num //= 10
+            return total_sum
+
+        slow = n
+        fast = get_next(n)
+        while fast != 1 and slow != fast:
+            slow = get_next(slow)
+            fast = get_next(get_next(fast))
+        return fast == 1
