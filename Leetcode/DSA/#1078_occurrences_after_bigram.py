d8e68a7f89f5697060c465dd90a7865862deb5d1
divijg19 <divijganjoo2003@gmail.com>
2026-03-15 23:00:00 +0530

    leetcode

---
+++ Leetcode/DSA/#1078_occurrences_after_bigram.py
@@ -0,0 +1,14 @@
+# 1078. Occurrences After Bigram
+# https://leetcode.com/problems/occurrences-after-bigram/
+
+from typing import List
+
+
+class Solution:
+    def findOcurrences(self, text: str, first: str, second: str) -> List[str]:
+        words = text.split()
+        res = []
+        for i in range(len(words) - 2):
+            if words[i] == first and words[i + 1] == second:
+                res.append(words[i + 2])
+        return res
---
+++ Leetcode/DSA/#3392_count_subarrays_of_length_three_with_a_condition.py
@@ -0,0 +1,13 @@
+# 3392. Count Subarrays of Length Three With a Condition
+# https://leetcode.com/problems/count-subarrays-of-length-three-with-a-condition/
+
+from typing import List
+
+
+class Solution:
+    def countSubarrays(self, nums: List[int]) -> int:
+        count = 0
+        for i in range(len(nums) - 2):
+            if 2 * (nums[i] + nums[i + 2]) == nums[i + 1]:
+                count += 1
+        return count
